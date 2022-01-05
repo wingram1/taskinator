1759107fae694b534cf0a7ccd6c299d0f54e24f2
wingram1 <waingram96@gmail.com>
2022-01-05 16:29:44 -0600

    added ability to add tasks and task types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var taskFormHandler = function(event) {
 
     event.preventDefault(); //prevents browser from refreshing by default when form is submitted
 
@@ -21,6 +21,32 @@ var createTaskHandler = function() {
 
     //adds entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+    //checks if both criteria have been inputted
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    //resets inputs to default
+    formEl.reset();
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
+    //create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+         
+    //add HTML content to div
+    listItemEl.appendChild(taskInfoEl);
+
+    //adds entire list item to list
+    tasksToDoEl.appendChild(listItemEl);    
+}
+
+formEl.addEventListener("submit", taskFormHandler);
